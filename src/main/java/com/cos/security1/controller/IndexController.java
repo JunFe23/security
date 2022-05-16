509f77f11267df1f02f1dcb825c748c16bdec00a
JunFe23 <ogum0405@hanmail.net>
2022-05-17 01:00:38 +0900

    Add: 시큐리티 회원가입

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,12 +1,24 @@
 package com.cos.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller// View를 리턴하겠다.
 public class IndexController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	// localhost:8080/
 	// localhost:8080
 	@GetMapping({"","/"})
@@ -31,19 +43,26 @@ public class IndexController {
 		return "manager";
 	}
 	
-	// 스프링시큐리티가 해당주소를 낚아채버림.
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	// 스프링시큐리티가 해당주소를 낚아채버림. - Security Config파일이 작동 안함.
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
 	
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "회원가입 완료됨.";
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user); // 회원가입이 잘됨. 비밀번호 : 1234 => 시큐리티로 로그인을 할 수 없음. 이유는 패스워드가 암호화가 안되었기 때문에.
+		return "redirect:/loginForm";
 	}
+
 }
