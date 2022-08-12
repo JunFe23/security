641a73d020fa162555d2da62eacbd9c24f6ade98
JunFe23 <ogum0405@hanmail.net>
2022-08-12 17:07:40 +0900

    구글 로그인 및 자동회원가입 진행완료(디버깅 필요)

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,25 +1,62 @@
 package com.cos.security1.config.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	// 구글로 부터 받은 userRequest 데이터에 대한 후처리되는 함수.
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		System.out.println("getClientRegistration : "+userRequest.getClientRegistration()); // registrationId로 어떤 OAuth로 로그인 했는지 확인.
 		System.out.println("getAccessToken : "+userRequest.getAccessToken());
+		
+		
+		OAuth2User oauth2User = super.loadUser(userRequest);
 		// 구글로그인 버튼 클릭 -> 구글로그인창 -> 로그인완료 -> code를 리턴(OAuth-Client라이브러리) -> AccessToken요청.
 		// userRequest정보 -> loadUser함수 -> 구글로부터 회원프로필 받아준다.
-		System.out.println("userRequest : "+super.loadUser(userRequest).getAttributes());
+		System.out.println("userRequest : "+oauth2User.getAttributes());
 		
-		OAuth2User oAuth2User = super.loadUser(userRequest);
 		// 회원가입을 강제로 진행.
-		return super.loadUser(userRequest);
+		String provider = userRequest.getClientRegistration().getClientId(); // google
+		String providerId = oauth2User.getAttribute("sub"); 
+		String username = provider+"_"+providerId; // google_113697389016560687183
+		String password = bCryptPasswordEncoder.encode("겟인데어");
+		String email = oauth2User.getAttribute("email"); 
+		String role = "ROLE_USER";
+		
+		User userEntity = userRepository.findByUsername(username);
+		
+		if(userEntity == null) {
+			userEntity = User.builder()
+					.username(username)
+					.password(password)
+					.email(email)
+					.role(role)
+					.provider(providerId)
+					.providerId(providerId)
+					.build();
+			userRepository.save(userEntity);
+		} else {
+			
+		}
+		
+		return new PrincipalDetails(userEntity,oauth2User.getAttributes());
 	}
 }
--- src/main/java/com/cos/security1/model/User.java
+++ src/main/java/com/cos/security1/model/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
@@ -26,4 +27,18 @@ public class User {
 	private String providerId;
 	@CreationTimestamp
 	private Timestamp createDate;
+	
+	@Builder
+	public User(String username, String password, String email, String role, String provider, String providerId,
+			Timestamp createDate) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+		this.provider = provider;
+		this.providerId = providerId;
+		this.createDate = createDate;
+	}
+	
+	
 }
